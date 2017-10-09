8df109cbd6e92baf72f6d9728b00368f2daeb01a
resurtm <resurtm@gmail.com>
2017-10-09 17:00:22 +0600

    integraet gui client with asyncio loop/client

--- weirdochat/client/main.py
+++ weirdochat/client/main.py
@@ -1,14 +1,23 @@
+import asyncio
 import sys
 
 from qtpy.QtWidgets import QApplication
+from quamash import QEventLoop
 
 from weirdochat.client.main_window import MainWindow
+from weirdochat.client.client import init_client
 
 
 def main():
     app = QApplication(sys.argv)
     main_window = MainWindow()
-    sys.exit(app.exec_())
+
+    # sys.exit(app.exec_())
+
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(init_client(loop))
+    loop.run_forever()
 
 
 if '__main__' == __name__:
